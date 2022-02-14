f2dfde79ae2509d190cc367ac7865f76bbf9ab42
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2022-02-14 10:19:19 +0900

    ex01domain完成

--- src/main/java/com/example/domain/Ex01Domain.java
+++ src/main/java/com/example/domain/Ex01Domain.java
@@ -2,4 +2,52 @@ package com.example.domain;
 
 public class Ex01Domain {
 
+	private Integer id;
+	private String leagueName;
+	private String teamName;
+	private String headquaters;
+	private String inauguration;
+	private String history;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLeagueName() {
+		return leagueName;
+	}
+	public void setLeagueName(String leagueName) {
+		this.leagueName = leagueName;
+	}
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public String getHeadquaters() {
+		return headquaters;
+	}
+	public void setHeadquaters(String headquaters) {
+		this.headquaters = headquaters;
+	}
+	public String getInauguration() {
+		return inauguration;
+	}
+	public void setInauguration(String inauguration) {
+		this.inauguration = inauguration;
+	}
+	public String getHistory() {
+		return history;
+	}
+	public void setHistory(String history) {
+		this.history = history;
+	}
+	@Override
+	public String toString() {
+		return "Ex01Domain [id=" + id + ", leagueName=" + leagueName + ", teamName=" + teamName + ", headquaters="
+				+ headquaters + ", inauguration=" + inauguration + ", history=" + history + "]";
+	}
+	
 }
